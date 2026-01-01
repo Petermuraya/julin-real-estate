0b13a32ea6aa5d577bc5f91adce32e833d16e908
Peter Muraya <sammypeter1944@gmail.com>
2026-01-01 09:10:08 +0300

    feat(layout): add (public) group layout to render Navbar and Footer

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,5 @@
+import PublicLayout from "@/ui/layouts/public.layout";
+
+export default function PublicSegmentLayout({ children }: { children: React.ReactNode }) {
+  return <PublicLayout>{children}</PublicLayout>;
+}
